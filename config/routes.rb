0c403adbe851bf8ca5cf3d73cddeea31b988d8f3
hiroyuki-ops <hiroyuki.1207.18@gmail.com>
2020-11-11 21:28:14 +0900

    favorite-posts

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :edit, :update, :destroy] do
     member do
      get :following, :followers
+     resources :favorites, only: [:index]
     end
   end
 
